11e4eab32b07bf29fcaf34f77441f32cb17623de
DaiShiLong123 <1315251041@qq.com>
2023-03-04 15:52:48 +0800

    登录功能-基本结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import './index.scss'
+import { Card } from 'antd'
+import logo from '../../assets/images/logo.png'
 
 export default class index extends Component {
   render() {
-    return <div>登录组件</div>
+    return (
+      <div className="login">
+        <Card className="login-container">
+          <img src={logo} className="login-logo"></img>
+        </Card>
+      </div>
+    )
   }
 }
